c6a5d983770967afe299f439916a9bcccdff488f
Nelson Penn <nelsonapenn@gmail.com>
2019-08-09 21:50:35 -0400

    added mongodb package, initialized client. Commented thread_local is an idea for accessing the connection across api calls.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+extern crate mongodb;
+
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+// use std::cell::RefCell;
+// 
+// thread_local!(static db:Client);
+
 fn index() -> impl Responder {
-      HttpResponse::Ok().body("Hello world!")
+  HttpResponse::Ok().body("Hello world!")
 }
 
 fn index2() -> impl Responder {
-      HttpResponse::Ok().body("Hello world again!")
+  HttpResponse::Ok().body("Hello world again!")
 }
 fn main() {
-      HttpServer::new(|| {
-                  App::new()
-                              .route("/", web::post().to(index))
-                                          .route("/again", web::post().to(index2))
-                                              })
-          .bind("127.0.0.1:8088")
-                .unwrap()
-                    .run()
-                        .unwrap();
+  // Direct connection to a server. Will not look for other servers in the topology.
+  let client = Client::connect("localhost", 27017)
+        .expect("Failed to initialize client.");
+  // db = client;
+
+
+  HttpServer::new(|| {
+      App::new()
+      .route("/", web::post().to(index))
+      .route("/again", web::post().to(index2))
+      })
+  .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
 }
